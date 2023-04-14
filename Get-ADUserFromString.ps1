8cfaf422f74e04679a0fab4ce7e54b7810ccc1b3
tiling-catwalk-liking <119548055+tiling-catwalk-liking@users.noreply.github.com>
2023-04-14 15:40:27 -0400

    Delete Get-ADUserFromString.ps1

--- Get-ADUserFromString.ps1
+++
@@ -1,8 +0,0 @@
-### This short script allows you to use Powershell to locate a user account name by entering a partial match of the users name.
-### The first variable is to get a string that is part of the users name
-$PartName = Read-Host -Prompt 'Enter part of the name of the user you are searching for'
-### $PartNameSearch variable takes the partial name string and puts wildcards on both sides so that it may be used
-### for the Get-ADUser search
-$PartNameSearch = '*' + $PartName + '*'
-### Show all users who share part of the string to be searched
-Get-ADUser -Filter 'Name -Like $PartNameSearch'
