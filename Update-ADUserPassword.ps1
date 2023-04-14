8ace0420b7841e7eb944cfe0bdad7137d3770976
tiling-catwalk-liking <119548055+tiling-catwalk-liking@users.noreply.github.com>
2023-04-14 15:44:06 -0400

    Delete Update-ADUserPassword.ps1

--- Update-ADUserPassword.ps1
+++
@@ -1,8 +0,0 @@
-### Work in progress for changing an AD user password by putting in SAMAccountName and staff ID (to generate password)
-
-$User = Read-Host -Prompt 'Enter username'
-$StaffID = Read-Host -Prompt 'Enter Staff_ID #'
-$NewPassword = 'Changeme!!!' + $StaffID
-
-Set-ADAccountPassword -Identity $User -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $NewPassword -force)
-Set-ADUser -Identity $User  -ChangePasswordAtLogon $true
