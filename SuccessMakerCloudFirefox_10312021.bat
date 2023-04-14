b9648462075c2160e0528bc57da0fc4629fa18f1
tiling-catwalk-liking <119548055+tiling-catwalk-liking@users.noreply.github.com>
2023-04-14 15:40:56 -0400

    Delete SuccessMakerCloudFirefox_10312021.bat

--- SuccessMakerCloudFirefox_10312021.bat
+++
@@ -1,7 +0,0 @@
-:: End any running Chrome process
-taskkill /im chrome.exe /f
-:: End any running Firefox process
-taskkill /im firefox.exe /f
-:: Launch Firefox in Kiosk Mode and go to SuccessMaker login
-cd "C:\Program Files\Mozilla Firefox"
-firefox.exe -kiosk "https://ourinstance.smhost.net/lms/sm.view?headless=1&action=home"
